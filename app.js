f74b5aef076af017e5473763c0efcfdf7e539939
derrick <derrick.hughes28@gmail.com>
2020-11-17 15:41:07 -0500

    lookin good boiiiii

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
+
+
 var adopt = [
     {
       title: 'Dog',
       image: "./assets/puppy-in-pet-store.png",
       price: '7,500',
       description: "BUY A PUPPY"
-
-    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
     },
+    {
+      title: 'Cat',
+      image: "./assets/kittens12.jpg",
+      price: '4,500',
+      description: "BUY A KITTEN"
+    },
+    {
+      title: 'Ferret',
+      image: "./assets/babyferretcute.jpg",
+      price: '1,500',
+      description: "BUY A BOX O' FERRETS"
+    },
+    
+    {
+      title: 'Cat?',
+      image: "./assets/angycat.jpg",
+      price: '3',
+      description: "PLEASE TAKE THIS ANGRY CAT. HE IS VERY MEAN"
+    }
+    
+  
     
     
   
@@ -33,10 +44,10 @@ var adopt = [
   
   
     for (var i=0; i < adopt.length ; i++){
-        var heading = '<div class="product '+ '"><span><h5>' + adopt[i].title + '</h5>'
-        var image = '<img src="' + adopt[i].image + '"/'
-        var price = '<p> $' + adopt[i].price + '</p></span>'
-        var description = '<div class="hoverProduct"><p>'+ adopt[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+        var heading = '<div class="product justify-content-center'+ '"><h5>' + adopt[i].title + '</h5>'
+        var image = '<img class="smaller mr-4" height="300px" width="450px" src="' + adopt[i].image + '"/>'
+        var price = '<p> $' + adopt[i].price + '</p>'
+        var description = '<div class="hoverProduct"><p>'+ adopt[i].description + '</p><button type="button" class="btn btn-success"> add to cart</button></div></div>'
         var concatThis = heading + image + price + description;
         postHTML = postHTML + concatThis
     }
